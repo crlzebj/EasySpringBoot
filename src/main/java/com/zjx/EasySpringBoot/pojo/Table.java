e49479da6a073dc8c7fa95d3e661a40a67ae475c
crlzebj <2393929142@qq.com>
2024-11-11 14:21:07 +0800

    添加注释

--- src/main/java/com/zjx/EasySpringBoot/pojo/Table.java
+++ src/main/java/com/zjx/EasySpringBoot/pojo/Table.java
@@ -9,11 +9,19 @@ import java.util.Map;
 
 @Data
 public class Table {
+    // MySQL 表名
     private String tableName;
+
+    // MySQL 表注释
     private String tableComment;
+
     private List<Field> fields;
     private Map<String, List<Field>> indexes;
+
+    // 表中是否含有Decimal数据类型
     private boolean hasDecimalType;
+
+    // 表中是否含有DateTime数据类型
     private boolean hasDateTimeType;
 
     public Table() {
